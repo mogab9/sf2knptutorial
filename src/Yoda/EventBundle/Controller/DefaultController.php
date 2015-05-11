ba8edba4cf9062bb04a1623181577723d16e833d
Gabriel Peyrot <gabriel.peyrot@citegreen.com>
2015-05-11 10:29:02 +0200

    Création du bundle knp pour tester twig

---
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Yoda\EventBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($count, $firstName)
+    {
+        return $this->render(
+            'EventBundle:Default:index.html.twig',
+            array(
+                'name'  => $firstName,
+                'count' => $count
+            )
+        );
+    }
+}
