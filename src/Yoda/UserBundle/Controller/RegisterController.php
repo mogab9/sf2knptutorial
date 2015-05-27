cba5145cf0ecdef3b5360ab744592b5e9f9b06d7
Gabriel Peyrot <gabriel.peyrot@citegreen.com>
2015-05-27 10:18:41 +0200

    Registration Form

---
+++ src/Yoda/UserBundle/Controller/RegisterController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Yoda\UserBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class RegisterController extends Controller
+{
+    /**
+     * @Route("/register", name="user_register")
+     * @Template
+     */
+    public function registerAction()
+    {
+        $form = $this->createFormBuilder()
+            ->add('username', 'text')
+            ->add('email', 'text')
+            ->add('password', 'password')
+            ->getForm()
+        ;
+
+        return compact($form);
+    }
+}
