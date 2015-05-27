0ed4f7d36adda69323f3a20ab12ee08f71767d42
Gabriel Peyrot <gabriel.peyrot@citegreen.com>
2015-05-27 11:31:13 +0200

    Handling Form Submissions

--- src/Yoda/UserBundle/Controller/RegisterController.php
+++ src/Yoda/UserBundle/Controller/RegisterController.php
@@ -4,8 +4,10 @@ namespace Yoda\UserBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Yoda\UserBundle\Entity\User;
 
 class RegisterController extends Controller
 {
@@ -13,7 +15,7 @@ class RegisterController extends Controller
      * @Route("/register", name="user_register")
      * @Template
      */
-    public function registerAction()
+    public function registerAction(Request $request)
     {
         $form = $this->createFormBuilder()
             ->add('username', 'text')
@@ -24,6 +26,31 @@ class RegisterController extends Controller
             ->getForm()
         ;
 
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $data = $form->getData();
+
+            $user = new User();
+            $user->setUsername($data['username']);
+            $user->setEmail($data['email']);
+            $user->setPassword($this->_encodePassword($user, $data['password']));
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $url = $this->generateUrl('event');
+            return $this->redirect($url);
+        }
         return array('form' => $form->createView());
+    }
+
+    private function _encodePassword(User $user, $plainPassword)
+    {
+        $encoder = $this->container->get('security.encoder_factory')
+            ->getEncoder($user)
+        ;
+
+        return $encoder->encodePassword($plainPassword, $user->getSalt());
     }
 }
