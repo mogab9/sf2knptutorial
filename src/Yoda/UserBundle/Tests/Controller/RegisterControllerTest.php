fde56deaccf127cc0a8a617f870be3db384110e6
Gabriel Peyrot <gabriel.peyrot@citegreen.com>
2015-05-29 16:06:46 +0200

    Functional Testing

---
+++ src/Yoda/UserBundle/Tests/Controller/RegisterControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Yoda\EventBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RegisterControllerTest extends WebTestCase
+{
+    public function testRegister()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/register');
+        $response = $client->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertContains('Register', $response->getContent());
+
+        $usernameVal = $crawler
+            ->filter('#user_register_username')
+            ->attr('value')
+        ;
+        $this->assertEquals('Foo', $usernameVal);
+    }
+}
