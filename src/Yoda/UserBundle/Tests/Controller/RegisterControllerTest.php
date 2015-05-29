0fed1f5a401aa99081116693d2d300d25e2b7eca
Gabriel Peyrot <gabriel.peyrot@citegreen.com>
2015-05-29 16:18:05 +0200

    Testing Forms

--- src/Yoda/UserBundle/Tests/Controller/RegisterControllerTest.php
+++ src/Yoda/UserBundle/Tests/Controller/RegisterControllerTest.php
@@ -21,5 +21,29 @@ class RegisterControllerTest extends WebTestCase
             ->attr('value')
         ;
         $this->assertEquals('Foo', $usernameVal);
+
+        $form = $crawler->selectButton('Register!')->form();
+
+        $crawler = $client->submit($form);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertRegexp(
+            '/Cette valeur ne doit pas être vide/',
+            $client->getResponse()->getContent()
+        );
+
+        $form = $crawler->selectButton('Register!')->form();
+
+        $form['user_register[username]']              = 'user6';
+        $form['user_register[email]']                 = 'user6@user.com';
+        $form['user_register[plainPassword][first]']  = 'P3ssword';
+        $form['user_register[plainPassword][second]'] = 'P3ssword';
+
+        $crawler = $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $client->followRedirect();
+        $this->assertContains(
+            'Welcome to the Death Star, have a magical day!',
+            $client->getResponse()->getContent()
+        );
     }
 }
